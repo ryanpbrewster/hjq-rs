536f589d8df4125efe21c70b34f78a667158a85c
Ryan P. Brewster <RyanPBrewster@gmail.com>
2018-10-17 00:19:16 -0700

    readability

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 extern crate serde;
 extern crate serde_json;
 
-use serde::de::{Deserialize, Deserializer, Visitor, MapAccess};
+use serde::de::{Deserialize, Deserializer, MapAccess, Visitor};
 use std::fmt;
 
 const TINY: &str = r#"
@@ -19,72 +19,65 @@ const TINY: &str = r#"
 
 static mut PREFIX: String = String::new();
 
-struct Rpb;
-impl<'de> Visitor<'de> for Rpb {
-    type Value = Rpb;
+struct Sentinel;
+impl<'de> Visitor<'de> for Sentinel {
+    type Value = Sentinel;
 
     fn expecting(&self, formatter: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         formatter.write_str("some bespoke garbage")
     }
 
-    fn visit_bool<E>(self, v: bool) -> Result<Self::Value, E>
-    {
-        println!("{} = {}", unsafe{&PREFIX}, v);
-        Ok(Rpb)
+    fn visit_bool<E>(self, v: bool) -> Result<Self::Value, E> {
+        println!("{} = {}", unsafe { &PREFIX }, v);
+        Ok(Sentinel)
     }
-    fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
-    {
-        println!("{} = {}", unsafe{&PREFIX}, v);
-        Ok(Rpb)
+    fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E> {
+        println!("{} = {}", unsafe { &PREFIX }, v);
+        Ok(Sentinel)
     }
-    fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
-    {
-        println!("{} = {}", unsafe{&PREFIX}, v);
-        Ok(Rpb)
+    fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E> {
+        println!("{} = {}", unsafe { &PREFIX }, v);
+        Ok(Sentinel)
     }
-    fn visit_f64<E>(self, v: f64) -> Result<Self::Value, E>
-    {
-        println!("{} = {}", unsafe{&PREFIX}, v);
-        Ok(Rpb)
+    fn visit_f64<E>(self, v: f64) -> Result<Self::Value, E> {
+        println!("{} = {}", unsafe { &PREFIX }, v);
+        Ok(Sentinel)
     }
-    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-    {
-        println!("{} = {}", unsafe{&PREFIX}, v);
-        Ok(Rpb)
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E> {
+        println!("{} = {}", unsafe { &PREFIX }, v);
+        Ok(Sentinel)
     }
-    fn visit_unit<E>(self) -> Result<Self::Value, E>
-    {
-        println!("{} = {}", unsafe{&PREFIX}, "null");
-        Ok(Rpb)
+    fn visit_unit<E>(self) -> Result<Self::Value, E> {
+        println!("{} = {}", unsafe { &PREFIX }, "null");
+        Ok(Sentinel)
     }
     fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
-        where
-            A: MapAccess<'de>,
+    where
+        A: MapAccess<'de>,
     {
         while let Some(k) = map.next_key::<&str>()? {
             unsafe {
                 PREFIX.push_str(k);
                 PREFIX.push('/');
             }
-            map.next_value::<Rpb>()?;
+            map.next_value::<Sentinel>()?;
             unsafe {
                 PREFIX.split_off(PREFIX.len() - k.len() - 1);
             }
         }
-        Ok(Rpb)
+        Ok(Sentinel)
     }
 }
 
-impl<'de> Deserialize<'de> for Rpb {
-    fn deserialize<D>(deserializer: D) -> Result<Rpb, D::Error>
-        where D: Deserializer<'de>
+impl<'de> Deserialize<'de> for Sentinel {
+    fn deserialize<D>(deserializer: D) -> Result<Sentinel, D::Error>
+    where
+        D: Deserializer<'de>,
     {
-        deserializer.deserialize_any(Rpb)
+        deserializer.deserialize_any(Sentinel)
     }
-
-
 }
 
 fn main() {
-    let _: Rpb = serde_json::from_str(TINY).unwrap();
+    let _: Sentinel = serde_json::from_str(TINY).unwrap();
 }
